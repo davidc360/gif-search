ceea8b7a36c85a5a89d01f929e45f726fe30e156
David <ddavado@gmail.com>
2021-01-20 18:26:00 -0500

    adding middleware

--- app.js
+++ app.js
@@ -5,8 +5,30 @@ const express = require('express');
 const app = express();
 
 // Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Routes
+app.get('/', (req, res) => {
+    // set the url of the gif
+    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+    // render the hello-gif view, passing the gifUrl into the view to be displayed
+    res.render('hello-gif', { gifUrl })
+})
+
+app.get('/:username', (req, res) => {
+    // Here you would look up the user from the database
+    // Then render the template to display the users's info
+})
+
+app.get('/greetings/:name', (req, res) => {
+    // grab the name from the path provided
+    const name = req.params.name;
+    // render the greetings view, passing along the name
+    res.render('greetings', { name });
+})
 
 // Start Server
 
@@ -14,13 +36,4 @@ app.listen(3000, () => {
   console.log('Gif Search listening on port localhost:3000!');
 });
 
-// Routes
-app.get('/:username', (req, res) => {
-    // Here you would look up the user from the database
-    // Then render the template to display the users's info
-})
-  
-// Routes
-app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
-  });
+
