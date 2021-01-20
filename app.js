f8ac884846ccd29dd92130faf52217fa81015b49
David <ddavado@gmail.com>
2021-01-20 18:18:08 -0500

    first route commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Require Libraries
+const express = require('express');
+
+// App Setup
+const app = express();
+
+// Middleware
+
+// Routes
+
+// Start Server
+
+app.listen(3000, () => {
+  console.log('Gif Search listening on port localhost:3000!');
+});
+
+// Routes
+app.get('/:username', (req, res) => {
+    // Here you would look up the user from the database
+    // Then render the template to display the users's info
+})
+  
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello Squirrel');
+  });
